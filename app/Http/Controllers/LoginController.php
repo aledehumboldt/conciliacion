4346117ba322e826167e99faaa9b37a303997f9f
aledehumboldt <ahumbo01@gmail.com>
2023-12-06 13:02:01 -0400

    error cambio de clave

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -46,7 +46,7 @@ class LoginController extends Controller
             $this->authenticated($request, $user);
 
             if(auth()->user()->estatus == 'Iniciado') {
-                return redirect('/reset')->with('mensaje','Por favor cambie su contraseña');
+                return redirect('/password')->with('mensaje','Por favor cambie su contraseña');
             }
 
             return redirect('/');
