985903dd1ee4d72f6635a9bdd88f13d134a704b4
aledehumboldt <ahumbo01@gmail.com>
2023-12-19 14:58:28 -0400

    estilos home y carousel

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -28,11 +28,6 @@
 		<link href="{{asset('css/animate.min.css')}}" rel="stylesheet">
 		<link href="{{asset('css/owl.carousel.min.css')}}" rel="stylesheet">
 		<link href="{{asset('css/stylo.css')}}" rel="stylesheet">
-    <style>
-      .offcanvas-md .offcanvas-body {
-        background-color: #fd585f !important;
-      }
-    </style>
     @yield('estilos')
 
   </head>
@@ -61,20 +56,20 @@
               data-bs-toggle="dropdown"
               aria-label="Toggle theme (auto)">
         <svg class="bi my-1 theme-icon-active" width="1em" height="1em"><use href="#circle-half"></use></svg>
-        <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
+        <span class="visually-hidden" id="bd-theme-text">Temas</span>
       </button>
       <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
         <li>
           <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
             <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="#sun-fill"></use></svg>
-            Light
+            Claro
             <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
           </button>
         </li>
         <li>
           <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
             <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="#moon-stars-fill"></use></svg>
-            Dark
+            Oscuro
             <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
           </button>
         </li>
