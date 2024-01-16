ea4d5bfa172c012cbb97bbeb3834f0152857ae13
aledehumboldt <ahumbo01@gmail.com>
2024-01-16 15:54:27 -0400

    Retoques de estilos

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -259,10 +259,10 @@
 <!--<a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>-->
 
 <!-- Footer Start -->
-<div class="container-fluid bg-dark text-secondary mt-5 pt-5">
+<div class="container-fluid bg-dark text-secondar mt-5 pt-5">
   <div class="row px-xl-5 pt-5">
     <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
-      <h5 class="text-secondary text-uppercase mb-4">Coordinacion de Aprovisionamiento</h5>
+      <h5 class="text-secondar text-uppercase mb-4">Coordinacion de Aprovisionamiento</h5>
       <p class="mb-4">Area encargada de llevar a cabo las comparaciones de distintas plataformas, con los facturadores, para realizar el aprovisionamiento correcto a los abonados, a nivel masivo.</p>
       <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>Calle Londres, Las Mercedes, Caracas</p>
       <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>conciliacionyaprovisionamiento@gmail.com</p>
@@ -272,29 +272,29 @@
     <div class="col-lg-8 col-md-12">
       <div class="row">
         <div class="col-md-4 mb-5">
-          <h5 class="text-secondary text-uppercase mb-4">Vacantes para la Coordinacion</h5>
+          <h5 class="text-secondar text-uppercase mb-4">Vacantes para la Coordinacion</h5>
           <div class="d-flex flex-column justify-content-start">
-            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Apoyo especializado</a>
-            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-            <a class="text-secondary" href="#"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+            <a class="text-secondar mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Apoyo especializado</a>
+            <a class="text-secondar mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
+            <a class="text-secondar mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
+            <a class="text-secondar mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
+            <a class="text-secondar mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
+            <a class="text-secondar" href="#"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
           </div>
         </div>
         <div class="col-md-4 mb-5">
-          <h5 class="text-secondary text-uppercase mb-4">My Account</h5>
+          <h5 class="text-secondar text-uppercase mb-4">My Account</h5>
           <div class="d-flex flex-column justify-content-start">
-            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Home</a>
-            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-            <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-            <a class="text-secondary" href="#"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
+            <a class="text-secondar mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Home</a>
+            <a class="text-secondar mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
+            <a class="text-secondar mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
+            <a class="text-secondar mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
+            <a class="text-secondar mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
+            <a class="text-secondar" href="#"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
           </div>
         </div>
         <div class="col-md-4 mb-5">
-          <h5 class="text-secondary text-uppercase mb-4">Newsletter</h5>
+          <h5 class="text-secondar text-uppercase mb-4">Newsletter</h5>
           <p>Duo stet tempor ipsum sit amet magna ipsum tempor est</p>
           <form action="">
             <div class="input-group">
@@ -304,7 +304,7 @@
               </div>
             </div>
           </form>
-          <h6 class="text-secondary text-uppercase mt-4 mb-3">Síguenos</h6>
+          <h6 class="text-secondar text-uppercase mt-4 mb-3">Síguenos</h6>
           <div class="d-flex">
             <a class="btn btn-secondary btn-square mr-2" href="https://twitter.com/SomosMovilnet_"><i class="fab fa-twitter"></i></a>
             <a class="btn btn-secondary btn-square mr-2" href="https://www.facebook.com/SomosMovilnetOFICIAL"><i class="fab fa-facebook-f"></i></a>
@@ -318,10 +318,10 @@
   </div>
   <div class="row border-top mx-xl-5 py-4" style="border-color: rgba(256, 256, 256, .1) !important;">
     <div class="col-md-6 px-xl-0">
-      <p class="mb-md-0 text-center text-md-left text-secondary">
-        &copy; <a class="text-primary" href="#">Domain</a>. Todos los Derechos Reservados. Desarrollado
+      <p class="mb-md-0 text-center text-md-left text-secondar">
+        <a class="text-primer" href="#">Domain</a>. Todos los Derechos Reservados. Desarrollado
         por
-        <strong class="text-primary">Aprovisionamiento</strong>
+        <strong class="text-primer">Aprovisionamiento</strong>
       </p>
     </div>
     <div class="col-md-6 px-xl-0 text-center text-md-right">
