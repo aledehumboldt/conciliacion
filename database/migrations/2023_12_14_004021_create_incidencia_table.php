2e4d2500c12cfe8369edc3df6843d8c6bdd13078
aledehumboldt <ahumbo01@gmail.com>
2024-01-22 09:29:47 -0400

    Agg confirm elim incide y tabla incide

--- database/migrations/2023_12_14_004021_create_incidencia_table.php
+++ database/migrations/2023_12_14_004021_create_incidencia_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void {
         Schema::create('incidencias', function (Blueprint $table) {
             $table->id();
-            $table->string('ticket');
+            $table->string('ticket')->unique();
             $table->string('inicio');
-            $table->string('fin');
+            $table->string('fin')->nullable();
             $table->text('descripcion');
             $table->text('solicitante');
             $table->timestamps();
